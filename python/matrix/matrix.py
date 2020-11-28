4f5ffcd61676445ce1a3b3a2b76558434247fe85
Neil Grogan <neil@grogan.ie>
2020-11-28 11:00:30 +0000

    Passing Matrix tests

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,24 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = []
+        if "\n" in matrix_string:
+            unproc_rows = matrix_string.split('\n')
+            for row in unproc_rows:
+                srow = list(map(int,row.split(' ')))
+                self.matrix.append(srow)
+        else:
+            nums = list(map(int, matrix_string.split(' ')))
+            row = []
+            for num in nums:
+                row.append(num)
+            self.matrix.append(row)
 
     def row(self, index):
-        pass
+        return self.matrix[index-1]
 
     def column(self, index):
-        pass
+        column = []
+        for row in self.matrix:
+            if row[index-1] is not None:
+                column.append(row[index-1])
+        return column
