93033d4bbc05306740f6ac79926065247d02b3c2
Neil Grogan <neil@grogan.ie>
2020-12-05 18:02:13 +0000

    Completed Python Hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,4 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Two strands must be equal length!")
+    return len([x for x, y in zip(strand_a, strand_b) if x != y])
