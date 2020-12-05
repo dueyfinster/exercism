6188e196147aa4bb4b0463ee40b488020dc6cd77
Neil Grogan <neil@grogan.ie>
2020-12-05 16:42:55 +0000

    Simplified Matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,16 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.matrix = []
-        if "\n" in matrix_string:
-            unproc_rows = matrix_string.split('\n')
-            for row in unproc_rows:
-                srow = list(map(int,row.split(' ')))
-                self.matrix.append(srow)
-        else:
-            nums = list(map(int, matrix_string.split(' ')))
-            row = []
-            for num in nums:
-                row.append(num)
+        lines = matrix_string.splitlines()
+        for line in lines:
+            row = list(map(int,line.split(' ')))
             self.matrix.append(row)
 
     def row(self, index):
