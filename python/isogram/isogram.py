0a6b2bb593ff9032210f299bb6e6f785651827d0
Neil Grogan <neil@grogan.ie>
2020-12-06 19:20:32 +0000

    Finish Isogram task

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,3 @@
 def is_isogram(string):
-    return len(set(string.lower())) == len(string)
+    alpha_string = ''.join(filter(str.isalpha, string))
+    return len(set(alpha_string.lower())) == len(alpha_string)
